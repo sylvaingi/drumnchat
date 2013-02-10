323d035cdb7448f10ae1ea8e35399976ac6c173d
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-02-10 13:02:46 +0100

    Streaming and waveform

--- client/views/player.js
+++ client/views/player.js
@@ -1,3 +1,5 @@
+var playingStream;
+
 _.extend(Template.player, {
     playing : function(){
         return Session.get("playing");
@@ -9,16 +11,31 @@ _.extend(Template.player, {
             return;
         }
 
+        if(playingStream){
+            playingStream.stop();
+        }
+
         var waveform = new Waveform({
             container: this.find(".waveform")
         });
+        waveform.dataFromSoundCloudTrack(playing);
+
+        var streamOptions = waveform.optionsForSyncedStream();
 
         var ctx = waveform.context;
-
         var gradient = ctx.createLinearGradient(0, 0, 0, waveform.height);
         gradient.addColorStop(0.0, "#f60");
         gradient.addColorStop(1.0, "#FF69B4");
 
-        waveform.dataFromSoundCloudTrack(playing);
+        streamOptions.innerColor = gradient;
+        streamOptions.autoPlay = true;
+        streamOptions.whileloading = _.wrap(streamOptions.whileloading, function(whileloading){
+            console.log(this.bytesLoaded);
+            whileloading.call(this);
+        });
+
+        SC.stream("/tracks/"+playing.id, streamOptions, function(s){
+            playingStream = s;
+        });
     }
 });
