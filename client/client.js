aa266a0a7c69941ee15dbd10572d0d96808b7004
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-02-11 22:18:03 +0100

    Client login overhaul

--- client/client.js
+++ client/client.js
@@ -1,16 +1,59 @@
-SC.storage = function(){
-    return localStorage;
-};
+(function(){
+    var subscribesIds = ["onair", "playlist", "allUserData"];
+    var subscribesHandles = [];
 
-SC.initialize({
-    client_id: DNC.SC.client_id,
-    redirect_uri: Meteor.absoluteUrl("soundcloud.html")
-});
+    function subscribeAll(){
+        _.each(subscribesIds, function(id){
+            var handle = Meteor.subscribe(id);
+            subscribesHandles.push(handle);
+        });
+    }
 
-Meteor.subscribe("playlist");
-Meteor.subscribe("onair");
-Meteor.subscribe("allUserData");
+    function unsubscribeAll(){
+        _.each(subscribesHandles, function(handle){
+            handle.stop();
+        });
+    }
 
-if(SC.isConnected()){
-    DNC.refreshUser();
-}
+    function refreshUser(){
+        SC.get("/me", function(me){
+            Session.set("user", me);
+            Meteor.call("heartbeat", Session.get("user"));
+
+            DNC.heartbeatId = Meteor.setInterval(function(){
+                Meteor.call("heartbeat");
+            }, 10000);
+
+            subscribeAll();
+        });
+    }
+
+    DNC.login = function(){
+        SC.connect(function(){
+            refreshUser();
+        });
+    };
+
+    DNC.logout = function(){
+        unsubscribeAll();
+
+        DNC.Player.destruct();
+        
+        Session.set("user", null);
+        Session.set("onair", null);
+
+        Meteor.clearInterval(DNC.heartbeatId);
+        Meteor.logout();
+
+        SC.disconnect();
+    };
+
+    SC.initialize({
+        client_id: DNC.SC.client_id,
+        redirect_uri: Meteor.absoluteUrl("soundcloud.html")
+    });
+
+    if(SC.isConnected()){
+        refreshUser();
+    }
+}());
--- client/views/connect.js
+++ client/views/connect.js
@@ -1,32 +1,20 @@
-DNC.refreshUser = function(){
-    SC.get("/me", function(me){
-        Session.set("user", me);
-        Meteor.call("heartbeat", Session.get("user"));
+(function(){
+    "use strict";
 
-        DNC.heartbeatId = Meteor.setInterval(function(){
-            Meteor.call("heartbeat");
-        }, 10000);
+    Template.connect.events({
+        "click .sc-connect": function(event, template){
+            event.preventDefault();
+            DNC.login();
+        },
+
+        "click .sc-disconnect": function(event, template){
+            event.preventDefault();
+            DNC.logout();
+        } 
     });
-};
 
+    Template.connect.user = function(){
+        return Session.get("user");
+    };
 
-Template.connect.events({
-    "click .sc-connect": function(event, template){
-        event.preventDefault();
-        SC.connect(function(){
-            DNC.refreshUser();
-        });
-    },
-
-    "click .sc-disconnect": function(event, template){
-        event.preventDefault();
-        SC.disconnect();
-        Session.set("user", null);
-        Meteor.clearInterval(DNC.heartbeatId);
-        Meteor.logout();
-    } 
-});
-
-Template.connect.user = function(){
-    return Session.get("user");
-};
+}());
