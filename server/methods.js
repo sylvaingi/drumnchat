9bcbbe5439efb8beb0ac7adb6d695a1b34e756cc
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-02-15 17:51:55 +0100

    Use soundcloud account package on server side

--- server/methods.js
+++ server/methods.js
@@ -4,12 +4,12 @@
             throw new Meteor.Error(403, "Please authenticate with Soundcloud first.");
         }    
     }
-
+ 
     Meteor.methods({
         "onAirOffset": function(){
             return DNC.Tracks.playingTrack().offset;
         },
-
+        
         "addTrack": function(url){
             console.log("Received SC URL '"+url+"'");
             ensureUserIsConnected(this.userId);
@@ -17,44 +17,10 @@
             DNC.Tracks.enqueue(url, this.userId);
         },
 
-        "vote": function(id){
-            console.log("Voting for "+id);
-            ensureUserIsConnected(this.userId);
-
-            DNC.Tracks.addVote(id, this.userId);
-        },
-
-        postMessage: function(msg){
-            ensureUserIsConnected(this.userId);
-            console.log("Incoming message from "+this.userId);
-
-            DNC.Chat.insert({message: msg, date: new Date(), user_id: this.userId});
-        },
-
         "heartbeat": function(data){
             console.log("User heartbeat: "+ (data? data.username + " (initial)" : this.userId));
-            this.unblock();
 
-            if (data){
-                var _id;
-                var previousUser = Meteor.users.findOne({username: data.username});
-                var user = _.pick(data, 'id', 'avatar_url', 'username', 'permalink_url');
-                user.lastSeen = new Date();
-                user.active = true;
-
-                if(previousUser){
-                    _id = previousUser._id;
-                    Meteor.users.update(_id, {$set: user});
-                }
-                else {
-                    _id = Meteor.users.insert(user);
-                }
-
-                this.setUserId(_id);
-            }
-            else {
-                Meteor.users.update(this.userId, {$set:{lastSeen: new Date(), active: true}});
-            }
+            Meteor.users.update(this.userId, {$set:{lastSeen: new Date(), active: true}});    
         }
     });
 }());
--- server/publications.js
+++ server/publications.js
@@ -2,8 +2,22 @@ Meteor.publish("playlist", function(){
     return DNC.Tracks.playlist();
 });
 
-Meteor.publish("allUserData", function () {
-    return Meteor.users.find({}, {fields:{'id':1, 'avatar_url':1, 'permalink_url':1, 'active':1, "username":1}});
+Meteor.publish("userData", function () {
+    var prefix = "services.soundcloud.";
+    var fieldSpec = _.reduce(
+        [
+            'id','username','permalink_url',
+            'avatar_url','country','full_name','city',
+            'description','website'
+        ], 
+        function(memo, field){
+            memo[prefix+field] = 1;
+            return memo;
+        }, 
+        {active:1}
+    );
+
+    return Meteor.users.find({}, {fields: fieldSpec});
 });
 
 Meteor.publish("chat", function(){
