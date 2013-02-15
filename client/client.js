8b6e680214d7a8f0b4c795fc6b476bcb5651fb11
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-02-15 21:31:20 +0100

    Proper user active state signalling

--- client/client.js
+++ client/client.js
@@ -4,28 +4,17 @@
     Meteor.subscribe("chat");
     Meteor.subscribe("userData");
 
-    function setUserActiveFlag(active){
-        Meteor.users.update({_id: Meteor.userId}, {$set:{"profile.active":active}});
-    }
-
-    function startSCPlayer(scData){
-        SC.initialize({
-            client_id: scData.clientId
-        });
-        DNC.Player.start();
-    }
-
     DNC.login = function(service){
-        var cb = function(){setUserActiveFlag(true);};
+        var cb = function(){Meteor.call("setActiveState", true);};
         switch(service){
             case "soundcloud":
-                Meteor.loginWithSoundcloud();
+                Meteor.loginWithSoundcloud(cb);
                 break;
             case "facebook":
-                Meteor.loginWithFacebook({});
+                Meteor.loginWithFacebook(cb);
                 break;
             case "google":
-                Meteor.loginWithGoogle({});
+                Meteor.loginWithGoogle(cb);
                 break;
             default:
                 alert("C'mon dude....");
@@ -36,20 +25,21 @@
     };
 
     DNC.logout = function(){
-        setUserActiveFlag(false);
+        Meteor.call("setActiveState", false);
         Meteor.logout();
     };
 
+    function startSCPlayer(scData){
+        SC.initialize({
+            client_id: scData.clientId
+        });
+        DNC.Player.start();
+    }
+    
     Accounts.loginServiceConfiguration.find({service: "soundcloud"})
         .observe({added: startSCPlayer});
 
     Meteor.setInterval(function(){
         Session.set("now", moment());
     }, 60000);
-
-    Meteor.autosubscribe(function(){
-        if(Meteor.userId()){
-            setUserActiveFlag(true);
-        }
-    });
 }());
