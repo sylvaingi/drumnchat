18a12dd1bed3f53b57a2d78d524f26b226c3bc1c
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-02-20 22:58:31 +0100

    Room creation for loggedin users

--- client/views/rooms.js
+++ client/views/rooms.js
@@ -1,3 +1,19 @@
+DNC.createRoom = function(roomName){
+    Meteor.call("createRoom", roomName, function(error, roomId){
+        if(roomId){
+            DNC.joinRoom(roomId);
+        }
+    });
+};
+
+Template.rooms.events({
+    submit: function(event, template){
+        event.preventDefault();
+        DNC.createRoom(template.find("input").value);
+        event.currentTarget.reset();
+    }
+});
+
 Template.rooms.helpers({
     rooms: DNC.Rooms.find({})
 });
